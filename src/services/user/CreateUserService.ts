590f1693b37e6e427465367fe2e952abe50bca9f
Heitor Augusto <heitorsantos377@gmail.com>
2024-08-24 12:03:23 -0300

    fix: implement encryption for password

--- src/services/user/CreateUserService.ts
+++ src/services/user/CreateUserService.ts
@@ -1,4 +1,5 @@
 import prismaClient from '../../prisma'
+import { hash } from 'bcryptjs'
 
 interface UserRequest{
     name: string
@@ -21,16 +22,19 @@ class CreateUserService{
             }
         })
 
+        
         if(userAlreadyExists){
             throw new Error("User already exists")
         }
-
+        
+        const passwordHash = await hash(password, 8)
+        
         const user = await prismaClient.user.create({
             data: {
                 name: name,
                 cpf: cpf,
                 email: email,
-                password: password
+                password: passwordHash
             },
             select: {
                 id: true,
