5e190d84519fe987c58057e1e8706d7ffc821994
Heitor Augusto <heitorsantos377@gmail.com>
2024-08-27 20:03:58 -0300

    feat: implement user login and JWT

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,12 @@
 import { Router } from 'express'
+
 import { CreateUserController } from './controllers/user/CreateUserController';
+import { AuthUserController } from './controllers/user/AuthUserController';
 
 const router = Router();
 
 router.post('/users', new CreateUserController().handle)
+
+router.get('/session', new AuthUserController().handle)
+
 export { router }
