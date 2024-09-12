5199b259e1c63dcda13b38fee133b784efa422f9
Heitor Augusto <heitorsantos377@gmail.com>
2024-09-12 19:47:59 -0300

    feat: create category route

--- src/router.ts
+++ src/router.ts
@@ -5,6 +5,7 @@ import { AuthUserController } from './controllers/user/AuthUserController';
 import { DetailUserController } from './controllers/user/DetailUserController';
 
 import { isAuthenticated } from './middlewares/isAuthenticated';
+import { CreateCategoryController } from './controllers/category/CreateCategoryController';
 
 const router = Router();
 
@@ -13,5 +14,7 @@ router.post('/users', new CreateUserController().handle)
 router.get('/session', new AuthUserController().handle)
 
 router.get('/me', isAuthenticated, new DetailUserController().handle)
+
+router.post('/category', isAuthenticated, new CreateCategoryController().handle)
 
 export { router }
