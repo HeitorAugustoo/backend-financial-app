45765d2ebe47403122f046cb4a26666fc204f99c
Heitor Augusto <heitorsantos377@gmail.com>
2024-11-17 12:38:00 -0300

    fix: correcting to send optional fields in req.body and expense validation with the same name

--- src/controllers/expense/AddExpenseController.ts
+++ src/controllers/expense/AddExpenseController.ts
@@ -4,7 +4,7 @@ import { AddExpenseService } from "../../services/expense/AddExpenseService";
 class AddExpenseController{
     async handle(req: Request, res: Response){
 
-        const { description, amount, due_date, category_id } = req.body
+        const { description, amount, due_date, category_id, ...optionalFields } = req.body
 
         const user_id = req.user_id
 
@@ -15,7 +15,8 @@ class AddExpenseController{
                 amount,
                 due_date,
                 category_id,
-                user_id
+                user_id,
+                ...optionalFields
         })
 
         return res.json(expense)
