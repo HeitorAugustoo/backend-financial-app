79c5834e609963a72655bef5c41ea2d06c92c55b
Heitor Augusto <heitorsantos377@gmail.com>
2024-11-23 12:28:05 -0300

    fix: get payment date when paying expenses

--- src/services/expense/PayExpenseService.ts
+++ src/services/expense/PayExpenseService.ts
@@ -2,21 +2,27 @@ import prismaClient from "../../prisma";
 
 interface ExpenseRequest{
     expense_id: string,
-    user_id: string
+    user_id: string,
+    date: string
 }
 
 class PayExpenseService{
-    async execute({expense_id, user_id}: ExpenseRequest){
+    async execute({expense_id, user_id, date}: ExpenseRequest){
+
+        if(!date){
+            throw new Error("Invalid payment date")
+        }
 
         const payExpense = await prismaClient.expense.update({
             where: {
                 id: expense_id,
-                userId: user_id
+                userId: user_id,
             },
             data: {
-                status: true
+                status: true,
+                payment_date: date
             }
-        })
+        })        
 
         return payExpense
     }
