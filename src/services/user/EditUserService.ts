523c4b40ee38c7e6b2199544f8d752d853c0681c
Heitor Augusto <heitorsantos377@gmail.com>
2024-12-03 21:36:08 -0300

    feat: user can change your password

--- src/services/user/EditUserService.ts
+++ src/services/user/EditUserService.ts
@@ -1,13 +1,16 @@
 import prismaClient from "../../prisma";
+import bcrypt from "bcryptjs"
 
-interface UserRequest{
+interface UserRequest {
     user_id: string,
     new_name: string,
     new_email: string
+    new_password: string
+    confirm_password: string
 }
 
-class EditUserService{
-    async execute({user_id, new_name, new_email}: UserRequest){
+class EditUserService {
+    async execute({ user_id, new_name, new_email, new_password, confirm_password }: UserRequest) {
 
         const nameOrEmailAlreadyExists = await prismaClient.user.findFirst({
             where: {
@@ -18,9 +21,19 @@ class EditUserService{
             }
         });
 
-        if(nameOrEmailAlreadyExists){
+        if (nameOrEmailAlreadyExists) {
             throw new Error("name or email already exists")
         }
+
+        if(new_password && !confirm_password){
+            throw new Error("confirming the new password is mandatory")
+        }
+
+        if(new_password !== confirm_password){
+            throw new Error("the passwords entered do not match")
+        }
+
+        const passwordHash = await bcrypt.hash(new_password, 8)
 
         const editUser = await prismaClient.user.update({
             where: {
@@ -28,7 +41,8 @@ class EditUserService{
             },
             data: {
                 name: new_name,
-                email: new_email
+                email: new_email,
+                password: passwordHash
             },
             select: {
                 id: true,
